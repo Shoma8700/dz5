f830b8962004424098b7db9c82a2b5ef64fe6a89
Shoma8700 <63722643+Shoma8700@users.noreply.github.com>
2023-09-29 00:46:34 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function PrototypeForm() {
+    const [form, setForm] = useState({
+        key1: 'Barbara',
+        key2: 'Hepworth',
+        key3: 'bhepworth@react.com',
+        key4: 'love cats'
+    })
+
+    function getChamge (event){
+        setForm({...form, key1: event.target.value})
+    }
+
+
+    return (
+        <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'space-around', backgroundColor: "#09cee0", width: "250px", padding: "20px", minHeight: "200px"}}>
+            Form:g
+           <label>
+               Firstname:
+               <input value={form.key1} onChange={getChamge}/>
+           </label>
+           <label>
+               Lastname:
+               <input value={form.key2} onChange={e => {
+                   setForm({...form, key2: e.target.value})
+               }}/>
+           </label>
+           <label>
+               email:
+               <input value={form.key3} onChange={e => {
+                   setForm({...form, key3: e.target.value})
+               }}/>
+           </label>
+
+           <label>
+               favorite enimal:
+               <input value={form.key4} onChange={e => {
+                   setForm({...form, key4: e.target.value})
+               }}/>
+           </label>
+           <p>
+               {form.key1} <br/> {form.key2} <br/> {form.key3} <br/> {form.key4}
+           </p>
+        </div>
+    )
 }
-
-export default App;
